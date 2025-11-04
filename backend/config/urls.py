6b0d1f27c90cc438fb47544c9471151c3a78f868
Facundo Nicolas Garcia Martoni <thunderxforce@gmail.com>
2025-11-04 15:13:29 -0300

    add hello world index.html

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,7 +19,10 @@ from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('api/', include('api.urls')),
