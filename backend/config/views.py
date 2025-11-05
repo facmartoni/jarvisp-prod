d9db2e9fca79f107d2ac14e4849b7d9930712620
Facundo Nicolas Garcia Martoni <thunderxforce@gmail.com>
2025-11-05 17:31:33 -0300

    implemented dynamic greeting in index.html depending environment (dev vs prod)

--- backend/config/views.py
+++ backend/config/views.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'is_dev': settings.DEBUG})
 
